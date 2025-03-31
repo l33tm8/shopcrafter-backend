e1aa9889cb829649fa4cb37d6bb2730767a434e1
Ilya <rzhunovskiy@gmail.com>
2025-03-31 21:42:56 +0500

    initial commit with empty core service

---
+++ shopcrafter-core/src/test/java/ru/ilya/shopcraftercore/ShopcrafterCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ilya.shopcraftercore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopcrafterCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
