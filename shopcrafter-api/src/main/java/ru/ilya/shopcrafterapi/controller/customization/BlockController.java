cee8bd601e5b7c895d2e695b92f4629826bd116e
Ilya <rzhunovskiy@gmail.com>
2025-05-14 00:04:10 +0500

    feat: block logic implementation

--- shopcrafter-api/src/main/java/ru/ilya/shopcrafterapi/controller/customization/BlockController.java
+++ shopcrafter-api/src/main/java/ru/ilya/shopcrafterapi/controller/customization/BlockController.java
@@ -1,9 +1,11 @@
 package ru.ilya.shopcrafterapi.controller.customization;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.ilya.shopcraftercore.dto.customization.BlockDto;
 import ru.ilya.shopcraftercore.dto.customization.PutPageDto;
 import ru.ilya.shopcraftercore.entity.customization.Block;
+import ru.ilya.shopcraftercore.service.customization.BlockService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,29 +14,36 @@ import java.util.List;
 @RequestMapping("/store/{storeId}/page/{pageId}/blocks")
 public class BlockController {
 
+    private final BlockService blockService;
+
+    @Autowired
+    public BlockController(final BlockService blockService) {
+        this.blockService = blockService;
+    }
+
     @GetMapping
     public List<BlockDto> getAllPageBlocks(@PathVariable long storeId, @PathVariable long pageId) {
-        return new ArrayList<>();
+        return blockService.getAllPageBlocks(storeId, pageId);
     }
 
     @GetMapping("/{blockId}")
     public BlockDto getPageBlock(@PathVariable long storeId, @PathVariable long pageId, @PathVariable long blockId) {
-        return new BlockDto();
+        return blockService.getPageBlock(storeId, pageId, blockId);
     }
 
     @PostMapping
-    public BlockDto createPageBlock(@PathVariable long storeId, @PathVariable long pageId, @RequestBody PutPageDto dto) {
-        return new BlockDto();
+    public BlockDto createPageBlock(@PathVariable long storeId, @PathVariable long pageId, @RequestBody BlockDto dto) {
+        return blockService.createPageBlock(storeId, pageId, dto);
     }
 
     @PutMapping("/{blockId}")
-    public BlockDto updatePageBlock(@PathVariable long storeId, @PathVariable long pageId, @PathVariable long blockId, @RequestBody PutPageDto dto) {
-        return new BlockDto();
+    public BlockDto updatePageBlock(@PathVariable long storeId, @PathVariable long pageId, @PathVariable long blockId, @RequestBody BlockDto dto) {
+        return blockService.updatePageBlock(storeId, pageId, blockId, dto);
     }
 
     @DeleteMapping("/{blockId}")
     public void deleteBlock(@PathVariable long storeId, @PathVariable long pageId, @PathVariable long blockId) {
-        return;
+        blockService.deletePageBlock(storeId, pageId, blockId);
     }
 
 }
--- shopcrafter-service/src/main/java/ru/ilya/shopcraftercore/service/customization/BlockService.java
+++ shopcrafter-service/src/main/java/ru/ilya/shopcraftercore/service/customization/BlockService.java
@@ -1,8 +1,107 @@
 package ru.ilya.shopcraftercore.service.customization;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.ilya.shopcraftercore.dto.customization.BlockDto;
+import ru.ilya.shopcraftercore.entity.customization.Block;
+import ru.ilya.shopcraftercore.entity.customization.BlockType;
+import ru.ilya.shopcraftercore.entity.goods.Store;
+import ru.ilya.shopcraftercore.entity.customization.Page;
+import ru.ilya.shopcraftercore.exception.EntityNotFoundException;
 import ru.ilya.shopcraftercore.repository.customization.BlockRepository;
+import ru.ilya.shopcraftercore.repository.customization.PageRepository;
+import ru.ilya.shopcraftercore.repository.goods.StoreRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BlockService {
+    private final BlockRepository blockRepository;
+    private final StoreRepository storeRepository;
+    private final PageRepository pageRepository;
+
+    @Autowired
+    public BlockService(BlockRepository blockRepository, StoreRepository storeRepository, PageRepository pageRepository) {
+        this.pageRepository = pageRepository;
+        this.storeRepository = storeRepository;
+        this.blockRepository = blockRepository;
+    }
+
+    public List<BlockDto> getAllPageBlocks(long storeId, long pageId) {
+        checkStoreExists(storeId);
+        Page page = getPageById(pageId);
+        List<Block> blocks = page.getBlocks();
+
+        return blocks
+                .stream()
+                .map(BlockDto::fromEntity)
+                .toList();
+    }
+
+    public BlockDto getPageBlock(long storeId, long pageId, long blockId) {
+        checkStoreExists(storeId);
+        getPageById(pageId);
+
+        Optional<Block> blockOptional = blockRepository.findById(blockId);
+        if (blockOptional.isEmpty()) {
+            throw new EntityNotFoundException("Block with id " + blockId + " not found");
+        }
+
+        Block block = blockOptional.get();
+        return BlockDto.fromEntity(block);
+    }
+
+    public BlockDto createPageBlock(long storeId, long pageId, BlockDto blockDto) {
+        checkStoreExists(storeId);
+        Page page = getPageById(pageId);
+        Block block = new Block();
+        block.setPage(page);
+        block.setName(blockDto.getName());
+        block.setType(BlockType.valueOf(blockDto.getBlockType()));
+        block.setParams(blockDto.getParams());
+        block = blockRepository.save(block);
+        return BlockDto.fromEntity(block);
+    }
+
+    public BlockDto updatePageBlock(long storeId, long pageId, long blockId, BlockDto blockDto) {
+        checkStoreExists(storeId);
+        getPageById(pageId);
+        Optional<Block> blockOptional = blockRepository.findById(blockId);
+        if (blockOptional.isEmpty()) {
+            throw new EntityNotFoundException("Block with id " + blockId + " not found");
+        }
+        Block block = blockOptional.get();
+        block.setName(blockDto.getName());
+        block.setType(BlockType.valueOf(blockDto.getBlockType()));
+        block.setParams(blockDto.getParams());
+        block = blockRepository.save(block);
+        return BlockDto.fromEntity(block);
+    }
+
+    public void deletePageBlock(long storeId, long pageId, long blockId) {
+        checkStoreExists(storeId);
+        getPageById(pageId);
+        Optional<Block> blockOptional = blockRepository.findById(blockId);
+        if (blockOptional.isEmpty()) {
+            throw new EntityNotFoundException("Block with id " + blockId + " not found");
+        }
+        Block block = blockOptional.get();
+        blockRepository.delete(block);
+    }
+
+    private void checkStoreExists(long storeId) {
+        Optional<Store> storeOptional = storeRepository.findById(storeId);
+        if (storeOptional.isEmpty()) {
+            throw new EntityNotFoundException("Store with id " + storeId + " not found");
+        }
+    }
+
+    private Page getPageById(long pageId) {
+        Optional<Page> pageOptional = pageRepository.findById(pageId);
+        if (pageOptional.isEmpty()) {
+            throw new EntityNotFoundException("Page with id " + pageId + " not found");
+        }
+        return pageOptional.get();
+    }
 }
